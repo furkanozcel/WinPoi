ba42592c7f4aba0dc9da04b6a78feac77fc6a6b5
HalilErenPab <erenpabuccu_2001@hotmail.com>
2024-11-08 17:04:09 +0300

    8.11.2024
    firebase auth çalışıyor ve google sing yapıldı

--- android/app/src/main/kotlin/com/example/winpoipo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/winpoipo/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.winpoipo
 
 import io.flutter.embedding.android.FlutterActivity
-import 'package:google_sign_in/google_sign_in.dart';
 
 class MainActivity : FlutterActivity() {
 }
